55ec593567b78c3206b299659867e7d6ddc14af3
Sergio Nezhigay <abc1971h@gmail.com>
2023-11-22 14:38:23 +0200

    add fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Devflow",
+  description:
+    "A communtity driven platform for asking and answering questions about",
+  icons: { icon: "/assets/images/site-logo.svg" },
+  // canonical: "https://clerk.dev",
+  // openGraph: {
+  //   url: "https://clerk.dev",
+  //   title: "Clerk NextJS",
+  //   description: "Clerk NextJS",
+  //   site_name: "Clerk NextJS",
+  // },
+  // twitter: {
+  //   handle: "@clerk",
+  //   site: "@clerk",
+  //   cardType: "summary_large_image",
+  // },
+};
 
 export default function RootLayout({
   children,
@@ -7,9 +41,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primart-gradients",
+          footerActionLink: "primart-text-gradients hover: text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
