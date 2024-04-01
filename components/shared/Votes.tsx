6dc072f2f9b13447b0cef077e7e2e911cbbdc79c
Sergio Nezhigay <abc1971h@gmail.com>
2024-04-01 10:59:15 +0300

    votes align

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -131,33 +131,39 @@ const Votes: React.FC<IVotes> = ({
 
   return (
     <div className="flex gap-5">
-      <p>{type}</p>
-      <Image
-        src={hasupVoted ? upvotedURL : upvoteURL}
-        width={18}
-        height={18}
-        onClick={() => handleVote("upvote")}
-        alt="up arrow"
-        className="cursor-pointer"
-      />
-      <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
-        <p className="subtle-medium text-dark400_light900">
-          {formatAndDivideNumber(upvotes)}
-        </p>
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={hasupVoted ? upvotedURL : upvoteURL}
+            width={18}
+            height={18}
+            onClick={() => handleVote("upvote")}
+            alt="up arrow"
+            className="cursor-pointer"
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+        <div className="flex-center gap-1.5">
+          <Image
+            src={hasdownVoted ? downvotedURL : downvoteURL}
+            width={18}
+            height={18}
+            alt="down arrow"
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
       </div>
-      <Image
-        src={hasdownVoted ? downvotedURL : downvoteURL}
-        width={18}
-        height={18}
-        alt="down arrow"
-        className="cursor-pointer"
-        onClick={() => handleVote("downvote")}
-      />
-      <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
-        <p className="subtle-medium text-dark400_light900">
-          {formatAndDivideNumber(downvotes)}
-        </p>
-      </div>
+
       {type === "Question" && (
         <>
           <Image
